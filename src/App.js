c8c839e10b36aa65e4301aa3918c75b7a177fe34
Artur Kosmatka <artur.kosmatka@gmail.com>
2021-07-26 20:54:48 +0100

    fixed issue with login page

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Home from './components/Home';
 import Detail from './components/Detail';
 import Login from './components/Login';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {selectUserName} from './features/user/userSlice';
+import {useSelector} from 'react-redux';
 
 function App() {
+  const userName = useSelector(selectUserName);
   return (
     <div className="App">
       <Router>
         <Header />
         <Switch>
-          <Route path="/login"><Login /></Route>
-          <Route path="/detail/:id"><Detail /></Route>
-          <Route path="/"><Home /></Route>
+          {!userName ?
+            <Route path="/login"><Login /></Route> :
+           <>
+            <Route path="/" exact><Home /></Route>
+            <Route path="/detail/:id"><Detail /></Route>
+            </>
+          }
         </Switch>
       </Router>
     </div>
